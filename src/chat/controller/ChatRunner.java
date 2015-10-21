53a78a66a8ae06ca4f12f0c2afad565b11a8edc9
josiahelmer <jelm9149@740s-pl-112.local>
2015-10-21 12:17:51 -0600

    Startup

---
+++ src/chat/controller/ChatRunner.java
@@ -0,0 +1,16 @@
+package chat.controller;
+
+/**
+ * 
+ * @author jelm9149
+ *
+ */
+public class ChatRunner
+{
+	public static void main(String[] args)
+	{
+		ChatController myChatController = new ChatController();
+		myChatController.start();
+	}
+	
+}
