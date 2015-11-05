219e93cd255a83b8c52a6407a616a2bf3baa3787
josiahelmer <jelm9149@740s-pl-112.local>
2015-11-05 12:31:51 -0700

    Added the constructor

--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -1,14 +1,23 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
-
+import javax.swing.ImageIcon;
 /**
  * 
  * @author jelm9149
  * @version 1.0 10/21/15
+ * @version 1.1 Added constructor
  */
 public class ChatView
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+	public ChatView()
+	{
+		windowMessage = "This message brought to you by the chatbot!";
+		chatIcon = new ImageIcon(getClass().getResource("image/VaultBoyIcon.png"));
+	}
 	/**
 	 * 
 	 * @param displayResponse 
